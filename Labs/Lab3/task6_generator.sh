b1f3ac7f042da9a9e23f7352afde7a501521b76c
Anastasia <113058066+nastyxxaavs@users.noreply.github.com>
2024-05-27 20:11:13 +0300

    Create task6_generator.sh

---
+++ Labs/Lab3/task6_generator.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+while true
+do
+read LINE
+case $LINE in
+TERM)
+kill -SIGTERM $(cat .pid)
+exit
+;;
+"+")
+kill -USR1 $(cat .pid)
+;;
+"*")
+kill -USR2 $(cat .pid)
+;;
+*)
+:
+;;
+esac 
+done
